b2acb13bc160f12be7510bf05b5ac9b3ad3db59a
mikaele <mikaele164@gmail.com>
2013-12-12 14:55:41 -0200

    transacao +conta

--- app/controllers/conta_controller.rb
+++ app/controllers/conta_controller.rb
@@ -89,6 +89,7 @@ class ContaController < ApplicationController
     @conta=Contum.find(params[:conta])
     if @conta.saldo.to_f-params[:saque].to_f>=0
       @conta.update_attribute(:saldo,@conta.saldo.to_f-params[:saque].to_f)
+      Transacao.create(:codigo=>DateTime.now.to_i, :data=>Date.today, :nro_conta=>params[:saque], :tipo=>'saque', :valor=>params[:saque])
       respond_to do |format|
         format.html { redirect_to "",alert: "Saque Efetuado com sucesso Saldo diponivel#{@conta.saldo}" }
         format.json { head :no_content }
@@ -100,6 +101,7 @@ class ContaController < ApplicationController
         @conta.saldo=0
       end
       @conta.update_attributes(:saldo=>@conta.saldo,:limite=>@conta.limite)
+      Transacao.create(:codigo=>DateTime.now.to_i, :data=>Date.today, :nro_conta=>params[:saque], :tipo=>'saque', :valor=>params[:saque])
       respond_to do |format|
         format.html { redirect_to "",alert: "Saque Efetuado com sucesso Saldo diponivel#{@conta.saldo}" }
         format.json { head :no_content }
