2bd77589585c5210a06a46664846a38209bc5e0d
mikaele <mikaele164@gmail.com>
2013-12-11 22:16:42 -0200

    duplo devise  para adm e hb

--- app/models/correntistum.rb
+++ app/models/correntistum.rb
@@ -1,4 +1,5 @@
 class Correntistum < ActiveRecord::Base
+  set_primary_key :cpf
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/funcionario.rb
+++ app/models/funcionario.rb
@@ -1,4 +1,5 @@
 class Funcionario < ActiveRecord::Base
+  set_primary_key :codigo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
